2906496bb7e092f83874a1c198b2cf733578b5bb
Fgc17 <fgcoelho1709@outlook.com>
2023-07-22 14:38:14 -0300

    style: formatting

--- apps/examples/src/getMembersAreaPage/example.ts
+++ apps/examples/src/getMembersAreaPage/example.ts
@@ -1,23 +1,23 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { HotmartClientConfig, HotmartClient } from "@glypho-hotmart-api/wrapper";
-import 'dotenv/config'
+import { HotmartClientConfig, HotmartClient } from "@glypho/hotmart-api-wrapper";
+import "dotenv/config";
 
 const clientConfig: HotmartClientConfig = {
-    environment: 'development',
-    secret: {
-        client_id: process.env.CLIENT_ID!,
-        client_secret: process.env.CLIENT_SECRET!,
-        basic: process.env.BASIC!
-    }
-}
+  environment: "development",
+  secret: {
+    client_id: process.env.CLIENT_ID!,
+    client_secret: process.env.CLIENT_SECRET!,
+    basic: process.env.BASIC!,
+  },
+};
 
 const hotmartClient = new HotmartClient(clientConfig);
 
 (async () => {
-    const pages = await hotmartClient.membersAreaService.getPages({
-        subdomain: 'my_subdomain',
-        module_id: '2Z7RAMXEJW'
-    })
+  const pages = await hotmartClient.membersAreaService.getPages({
+    subdomain: "my_subdomain",
+    module_id: "2Z7RAMXEJW",
+  });
 
-    console.log(pages)
-})()
+  console.log(pages);
+})();
--- apps/hotmart-api-wrapper/src/services/SubscriptionService.ts
+++ apps/hotmart-api-wrapper/src/services/SubscriptionService.ts
@@ -7,43 +7,37 @@ import HotmartTypes from "@glypho/hotmart-api-types";
 import { HotmartEndpointsService } from "@glypho/hotmart-api-endpoints";
 
 export class SubscriptionService {
+  private accessTokenObjectService: AccessTokenObjectService;
+  private endpointsService: HotmartEndpointsService;
 
-    private accessTokenObjectService: AccessTokenObjectService
-    private endpointsService: HotmartEndpointsService
+  constructor(private apiContext: APIContext) {
+    this.accessTokenObjectService = new AccessTokenObjectService(this.apiContext);
+    this.endpointsService = new HotmartEndpointsService(this.apiContext.environment);
+  }
 
-    constructor(
-        private apiContext: APIContext
-    ) {
-        this.accessTokenObjectService = new AccessTokenObjectService(this.apiContext)
-        this.endpointsService = new HotmartEndpointsService(this.apiContext.environment)
-    }
+  async get(params: Partial<HotmartTypes.API.Subscription.SubscriptionsGetRequestParameters>) {
+    const accessTokenObject = await this.accessTokenObjectService.getValid();
 
-    async get(params: Partial<HotmartTypes.API.Subscription.SubscriptionsGetRequestParameters>) {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + accessTokenObject.access_token,
+    };
 
-        const accessTokenObject = await this.accessTokenObjectService.getValid()
+    const endpoint = this.endpointsService.getEndpoint("subscriptions", "get");
 
-        const headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + accessTokenObject.access_token
-        }
+    const getSubscriptionResData: HotmartTypes.API.Subscription.SubscriptionsGetRequestResponseData = await axios({
+      ...endpoint,
+      headers: headers,
+      params: params,
+    })
+      .then(({ data }) => {
+        const typedData: HotmartTypes.API.Subscription.SubscriptionsGetRequestResponseData = data;
+        return typedData;
+      })
+      .catch((err) => {
+        throw err;
+      });
 
-        const endpoint = this.endpointsService.getEndpoint('subscriptions', "get")
-
-        const getSubscriptionResData: HotmartTypes.API.Subscription.SubscriptionsGetRequestResponseData = await axios({
-            ...endpoint,
-            headers: headers,
-            params: params
-        }
-        )
-            .then(({ data }) => {
-                const typedData: HotmartTypes.API.Subscription.SubscriptionsGetRequestResponseData = data
-                return typedData
-            })
-            .catch(err => {
-                throw err
-            })
-
-        return getSubscriptionResData
-    }
-
+    return getSubscriptionResData;
+  }
 }
