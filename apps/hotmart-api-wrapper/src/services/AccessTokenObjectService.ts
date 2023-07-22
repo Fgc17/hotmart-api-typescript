dd245db6c727b7f67b119a83dbb73c6cbbbde4e6
Fgc17 <fgcoelho1709@outlook.com>
2023-07-22 14:32:24 -0300

    fix: now uses new secret instead of caching old one

--- apps/hotmart-api-wrapper/src/services/AccessTokenObjectService.ts
+++ apps/hotmart-api-wrapper/src/services/AccessTokenObjectService.ts
@@ -1,98 +1,104 @@
-
-// Flat Cache 
-import * as flatCache from 'flat-cache'
+// Flat Cache
+import * as flatCache from "flat-cache";
 
 // Axios
 import axios from "axios";
 
 // Types
-import { APIContext } from '../types/ApiContext';
+import { APIContext } from "../types/ApiContext";
 
 // Hotmart API Packages
-import HotmartTypes from "@glypho-hotmart-api/types";
-import { HotmartEndpointsService } from '@glypho-hotmart-api/endpoints';
+import HotmartTypes from "@glypho/hotmart-api-types";
+import { HotmartEndpointsService } from "@glypho/hotmart-api-endpoints";
 
 export class AccessTokenObjectService {
+  private cache: flatCache.Cache;
+  private endpointsService: HotmartEndpointsService;
 
-    private cache: flatCache.Cache
-    private endpointsService: HotmartEndpointsService
+  constructor(private apiContext: APIContext) {
+    this.cache = flatCache.load("hotmartCache");
+    this.endpointsService = new HotmartEndpointsService(this.apiContext.environment);
+  }
 
-    constructor(
-        private apiContext: APIContext
-    ) {
-        this.cache = flatCache.load('hotmartAccessToken')
-        this.endpointsService = new HotmartEndpointsService(this.apiContext.environment)
+  public async getValid(): Promise<HotmartTypes.Entity.AccessTokenObject> {
+    const currentAccessTokenObject = this.get();
+
+    const isCurrentAccessTokenObjectValid = this.validate(currentAccessTokenObject);
+
+    const currentSecret = this.getSecret();
+
+    const compareSecrets = JSON.stringify(currentSecret) === JSON.stringify(this.apiContext.secret);
+
+    if (!isCurrentAccessTokenObjectValid || !compareSecrets) {
+      const newAccessToken = await this.generate();
+      this.set(newAccessToken);
+      return newAccessToken;
     }
 
-    public async getValid(): Promise<HotmartTypes.Entity.AccessTokenObject> {
+    return currentAccessTokenObject;
+  }
 
-        const currentAccessTokenObject = this.get()
+  private async generate(): Promise<HotmartTypes.Entity.AccessTokenObject> {
+    const { basic, ...tokenUrlParams } = this.apiContext.secret;
 
-        const isCurrentAccessTokenObjectValid = this.validate(currentAccessTokenObject)
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: basic,
+    };
 
-        if (!isCurrentAccessTokenObjectValid) {
-            const newAccessToken = await this.generate()
-            this.set(newAccessToken)
-            return newAccessToken
-        }
+    const params = {
+      grant_type: "client_credentials",
+      ...tokenUrlParams,
+    };
 
-        return currentAccessTokenObject
-    }
+    const endpoint = this.endpointsService.getEndpoint("authentication", "getAccessObject");
 
-    private async generate(): Promise<HotmartTypes.Entity.AccessTokenObject> {
-        const { basic, ...tokenUrlParams } = this.apiContext.secret
+    const accessTokenObject: HotmartTypes.API.Authentication.AccessTokenGetRequestResponseData | void = await axios({
+      ...endpoint,
+      headers: headers,
+      params: params,
+    })
+      .then(({ data }): HotmartTypes.API.Authentication.AccessTokenGetRequestResponseData => data)
+      .catch(() => {
+        throw new Error("Confira suas credenciais de acesso. Erro 401.");
+      });
 
-        const headers = {
-            'Content-Type': 'application/json',
-            'Authorization': basic
-        }
+    const expiryDate = new Date().getTime() + accessTokenObject.expires_in;
 
-        const params = {
-            grant_type: "client_credentials",
-            ...tokenUrlParams
-        }
+    return {
+      ...accessTokenObject,
+      expiryDate: expiryDate,
+    };
+  }
 
-        const endpoint = this.endpointsService.getEndpoint('authentication', "getAccessObject")
+  private validate(accessTokenObject: HotmartTypes.Entity.AccessTokenObject) {
+    if (!accessTokenObject) return false;
 
-        const accessTokenObject: HotmartTypes.API.Authentication.AccessTokenGetRequestResponseData | void = await axios({
-            ...endpoint,
-            headers: headers,
-            params: params
-        })
-            .then(({ data }): HotmartTypes.API.Authentication.AccessTokenGetRequestResponseData => data)
-            .catch(err => {
-                throw err
-            })
+    const currentDate = new Date().getTime();
 
-        const expiryDate = new Date().getTime() + accessTokenObject.expires_in
+    if (currentDate > accessTokenObject.expiryDate) return false;
 
-        return {
-            ...accessTokenObject,
-            expiryDate: expiryDate
-        }
-    }
+    return true;
+  }
 
-    private validate(accessTokenObject: HotmartTypes.Entity.AccessTokenObject) {
+  private setSecret(secret: HotmartTypes.Entity.Secret) {
+    this.cache.setKey("secret", secret);
+    this.cache.save(true);
+  }
 
-        if (!accessTokenObject) return false
+  private getSecret(): HotmartTypes.Entity.Secret {
+    return this.cache.getKey("secret");
+  }
 
-        const currentDate = new Date().getTime()
+  private set(accessTokenObject: HotmartTypes.Entity.AccessTokenObject) {
+    this.cache.setKey("accessTokenObject", {
+      ...accessTokenObject,
+      expiryDate: accessTokenObject.expires_in + new Date().getTime(),
+    });
+    this.cache.save(true);
+  }
 
-        if (currentDate > accessTokenObject.expiryDate) return false
-
-        return true
-    }
-
-    private set(accessTokenObject: HotmartTypes.Entity.AccessTokenObject) {
-        this.cache.setKey('accessTokenObject', {
-            ...accessTokenObject,
-            expiryDate: accessTokenObject.expires_in + new Date().getTime()
-        })
-        this.cache.save(true)
-    }
-
-    private get() {
-        return this.cache.getKey('accessTokenObject')
-    }
-
+  private get(): HotmartTypes.Entity.AccessTokenObject {
+    return this.cache.getKey("accessTokenObject");
+  }
 }
