d9fc5e2409b07ccd4b2e3afb1dc1bcb27e0505b1
Fgc17 <fgcoelho1709@outlook.com>
2023-07-30 14:02:50 -0300

    feat: simpler types and now uses the default fetch js api

--- packages/endpoints/index.ts
+++ packages/endpoints/index.ts
@@ -1,108 +1,140 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Hotmart API Packages
 import HotmartTypes from "@glypho/hotmart-api-types";
 
-type HttpMethods = 'get' | 'post' | 'put' | 'delete' | 'patch';
+type HttpMethod = "get" | "post" | "put" | "delete" | "patch";
 
-interface Endpoint<Params extends string | undefined = undefined> {
-  params?: Params[];
-  baseURL?: string;
+type ResourcesKeys = keyof Endpoints;
+type EndpointKeys<T extends ResourcesKeys> = keyof Endpoints[T];
+type Params<T extends ResourcesKeys, U extends EndpointKeys<T>> = "params" extends keyof Endpoints[T][U]
+  ? Endpoints[T][U]["params"]
+  : never;
+
+type Endpoint = {
+  baseURL?: HotmartTypes.API.EnvironmentUrl;
   url: string;
-  method: HttpMethods;
-}
+  method: HttpMethod;
+};
 
-interface Resources {
-  authentication: {
-    getAccessObject: Endpoint
-  },
-  subscriptions: {
-    get: Endpoint
-  },
+type Endpoints = {
   membersArea: {
-    getModules: Endpoint
-    getPages: Endpoint<'module_id'>
-    getStudents: Endpoint
-  }
-}
-
-type ParamObject<T extends Endpoint<any>> = T extends Endpoint<infer Params> ? {
-  [K in NonNullable<Params>]: string;
-} : object
-
+    getModules: {
+      url: "club/api/v1/modules?subdomain=:subdomain&is_extra=:is_extra";
+      method: "get";
+      params?: HotmartTypes.API.MembersArea.ModulesGetRequest;
+    };
+    getPages: {
+      url: `club/api/v1/modules/:module_id/pages`;
+      method: "get";
+      params?: HotmartTypes.API.MembersArea.PagesGetRequest;
+    };
+    getStudents: {
+      url: "students";
+      method: "get";
+    };
+  };
+  authentication: {
+    getAccessObject: {
+      baseURL: "https://api-sec-vlc.hotmart.com";
+      url: "security/oauth/token";
+      method: "post";
+      params?: HotmartTypes.API.Authentication.AccessTokenObjectPostRequest;
+    };
+  };
+  subscriptions: {
+    get: {
+      url: "payments/api/v1/subscriptions";
+      method: "get";
+      params?: HotmartTypes.API.Subscription.SubscriptionsGetRequest;
+    };
+  };
+};
 
 export class HotmartEndpointsService {
-
-  private authenticationEndpoints: Resources['authentication'] = {
-    getAccessObject: {
-      baseURL: 'https://api-sec-vlc.hotmart.com',
-      url: 'security/oauth/token',
-      method: "post",
-    }
-  };
-
-  private subscriptionEndpoints: Resources['subscriptions'] = {
-    get: {
-      url: "payments/api/v1/subscriptions",
-      method: "get",
-    }
-  };
-
-  private membersAreaEndpoints: Resources['membersArea'] = {
-    getModules: {
-      url: 'club/api/v1/modules/',
-      method: "get",
+  private endpoints: Endpoints = {
+    membersArea: {
+      getModules: {
+        url: "club/api/v1/modules?subdomain=:subdomain&is_extra=:is_extra",
+        method: "get",
+      },
+      getPages: {
+        url: `club/api/v1/modules/:module_id/pages`,
+        method: "get",
+      },
+      getStudents: {
+        url: "students",
+        method: "get",
+      },
     },
-    getPages: {
-      url: `club/api/v1/modules/:module_id/pages`,
-      method: "get",
+    authentication: {
+      getAccessObject: {
+        baseURL: "https://api-sec-vlc.hotmart.com",
+        url: "security/oauth/token",
+        method: "post",
+      },
     },
-    getStudents: {
-      url: "/students",
-      method: "get",
-    }
+    subscriptions: {
+      get: {
+        url: "payments/api/v1/subscriptions",
+        method: "get",
+      },
+    },
   };
 
-  private hotmartEndpoints: Resources = {
-    authentication: this.authenticationEndpoints,
-    subscriptions: this.subscriptionEndpoints,
-    membersArea: this.membersAreaEndpoints
-  };
-
-  private baseURL: HotmartTypes.API.EnvironmentUrl
+  private baseURL: HotmartTypes.API.EnvironmentUrl;
 
   constructor(private environment: HotmartTypes.API.Environment) {
-    this.environment === 'production' ? this.baseURL = 'https://developers.hotmart.com' : this.baseURL = 'https://sandbox.hotmart.com'
+    this.baseURL = environment === "production" ? "https://developers.hotmart.com" : "https://sandbox.hotmart.com";
   }
 
-  getEndpoint<T extends keyof Resources, P extends keyof Resources[T], K extends ParamObject<Resources[T][P]>>(
-    resource: T,
-    endpoint: P,
-    params?: K
+  requestBuilder<R extends ResourcesKeys, E extends EndpointKeys<R>>(
+    resourceKey: R,
+    endpointKey: E,
+    params: Partial<Params<R, E>> & {
+      authKey: string;
+    }
   ) {
-    const _resource: Resources[T] = this.hotmartEndpoints[resource]
-    const _endpoint: Endpoint = _resource[endpoint]
+    const resource = this.endpoints[resourceKey];
+    const endpoint: Endpoint = resource[endpointKey];
 
-    if (!_endpoint.baseURL) _endpoint.baseURL = this.baseURL
+    if (!endpoint?.baseURL) endpoint.baseURL = this.baseURL;
 
-    if (params) _endpoint.url = this.applyEndpointParams(_endpoint, params)
+    const request = this.generateRequest(endpoint, params);
 
-    return _endpoint
+    return request;
   }
 
+  private generateRequest(endpoint: Endpoint, params: any) {
+    const compositionParams = params.url_params?.composition;
+    const queryParams = params.url_params?.query;
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: params?.authKey || "",
+    };
+    const body = params.body;
 
-  private applyEndpointParams<E extends Endpoint, P extends ParamObject<E>>(
-    endpoint: E, params: P
-  ) {
-    const endpointParams = Object.keys(params) as Array<keyof P>;
-
-    if (endpointParams.length > 0) {
-      endpointParams.forEach((key) => {
-        const paramValue = params[key]
-        if (paramValue !== undefined) {
-          endpoint.url = endpoint.url.replace(`:${String(key)}`, paramValue)
-        }
+    if (compositionParams) {
+      Object.keys(compositionParams).forEach((key) => {
+        endpoint.url = endpoint.url.replace(`:${key}`, compositionParams[key]);
       });
     }
 
-    return endpoint.url;
+    if (queryParams) {
+      const queryString = Object.keys(queryParams)
+        .map((key) => `${key}=${queryParams[key]}`)
+        .join("&");
+      endpoint.url = `${endpoint.url}?${queryString}`;
+    }
+
+    const request = {
+      init: {
+        method: endpoint.method,
+        headers: headers,
+        body: body,
+      },
+      url: `${endpoint.baseURL}/${endpoint.url}`,
+    };
+
+    return request;
   }
 }
